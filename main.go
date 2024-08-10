0f5f6d07758135bc62c733b7f8940e35e93cc35b
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2024-08-10 23:56:29 +0200

    (feat) Implements basic xml skeloton

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+)
+
+const (
+	ExtensionApplication = "http://www.videolan.org/vlc/playlist/0"
+	Xmlns                = "http://xspf.org/ns/0/"
+	XmlnsVlc             = "http://www.videolan.org/vlc/playlist/ns/0/"
+)
+
+type Extension struct {
+	XMLName     xml.Name `xml:"extension"`
+	Application string   `xml:"application,attr"`
+	Id          string   `xml:"vlc:id"`
+}
+
+type Track struct {
+	XMLName  xml.Name  `xml:"track"`
+	Location string    `xml:"location"`
+	Ext      Extension `xml:"extension"`
+}
+
+type TrackList struct {
+	XMLName xml.Name `xml:"trackList"`
+	Tracks  []*Track `xml:"track"`
+}
+
+type PlayList struct {
+	XMLName  xml.Name  `xml:"playlist"`
+	Xmlns    string    `xml:"xmlns,attr"`
+	XmlnsVlc string    `xml:"xmlns:vlc,attr"`
+	Version  string    `xml:"version,attr"`
+	Title    string    `xml:"title"`
+	Tl       TrackList `xml:"trackList"`
+}
+
+func main() {
+	ext := &Extension{Application: ExtensionApplication, Id: "01"}
+	extOut, err := xml.MarshalIndent(ext, "", "  ")
+	if err != nil {
+		fmt.Printf("fix this later, now error: %s\n", err)
+	}
+	fmt.Println(string(extOut))
+
+	track1 := &Track{Location: "/mnt/path/", Ext: *ext}
+	out, err := xml.MarshalIndent(track1, " ", "  ")
+	if err != nil {
+		fmt.Printf("fix this later, now error: %s\n", err)
+	}
+	fmt.Println(string(out))
+	track2 := &Track{Location: "/mnt/path/", Ext: Extension{Application: ExtensionApplication, Id: "02"}}
+
+	tl := &TrackList{}
+	tl.Tracks = []*Track{track1, track2}
+
+	tlOut, err := xml.MarshalIndent(tl, " ", "  ")
+	if err != nil {
+		fmt.Printf("fix this later, now error: %s\n", err)
+	}
+	fmt.Println(string(tlOut))
+
+	pl := &PlayList{Xmlns: Xmlns, XmlnsVlc: XmlnsVlc, Version: "1", Title: "Test playlist", Tl: *tl}
+	plOut, err := xml.MarshalIndent(pl, " ", "  ")
+	if err != nil {
+		fmt.Printf("fix this later, now error: %s\n", err)
+	}
+	fmt.Println(xml.Header + string(plOut))
+}

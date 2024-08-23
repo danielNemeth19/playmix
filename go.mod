60a20f1f39507523aea704795d4e8eab89341c66
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2024-08-24 00:57:58 +0200

    (feat) Tries to get metadata from video files, without luck

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module playmix
 
 go 1.22.3
+
+require github.com/abema/go-mp4 v1.2.0
+
+require github.com/google/uuid v1.6.0 // indirect
--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	// "bufio"
 	"encoding/xml"
 	"fmt"
 	"log"
 	"os"
-    "path/filepath"
+	"path/filepath"
+
+	"github.com/abema/go-mp4"
 )
 
 const (
@@ -48,54 +51,54 @@ type FolderContent struct {
 }
 
 func isVideoFile(ext string) bool {
-    for _, v := range videoExtensions {
-        if v == ext {
-            return true
-        }
-    }
-    return false
+	for _, v := range videoExtensions {
+		if v == ext {
+			return true
+		}
+	}
+	return false
 }
 
 func collectExtensions(p string, extensions *[]string, seen *map[string]bool) *[]string {
-    fmt.Println("Checking folder: ", p)
-    files, err := os.ReadDir(p) 
-    if err != nil {
-        log.Fatalf("Error raised: %s\n", err)
-    }
-    for _, obj := range files {
-        if obj.IsDir() {
-            extensions = collectExtensions(p + "/" + obj.Name(), extensions, seen)
-        } else {
-            extension := filepath.Ext(obj.Name())
-            if !(*seen)[extension] {
-                fmt.Printf("Seen first: %s -- %s, %v\n", extension, obj.Name(), obj.IsDir())
-                (*seen)[extension] = true
-                *extensions = append(*extensions, extension)
-            }
-        }
-    }
-    return extensions
+	fmt.Println("Checking folder: ", p)
+	files, err := os.ReadDir(p)
+	if err != nil {
+		log.Fatalf("Error raised: %s\n", err)
+	}
+	for _, obj := range files {
+		if obj.IsDir() {
+			extensions = collectExtensions(p+"/"+obj.Name(), extensions, seen)
+		} else {
+			extension := filepath.Ext(obj.Name())
+			if !(*seen)[extension] {
+				fmt.Printf("Seen first: %s -- %s, %v\n", extension, obj.Name(), obj.IsDir())
+				(*seen)[extension] = true
+				*extensions = append(*extensions, extension)
+			}
+		}
+	}
+	return extensions
 }
 
 func getFolderContent(p string) ([]FolderContent, error) {
-    extensions := []string{}
-    seen := map[string]bool{}
-    files, err := os.ReadDir(p)
-    if err != nil {
-        return nil, fmt.Errorf("Error raised: %w\n", err)
-    }
-    var content []FolderContent
-    for i, file := range files {
-        extension := filepath.Ext(file.Name())
-        fmt.Printf("File %d: %s, extension: %s\n", i, file.Name(), extension)
-        fmt.Printf("Verdict: %v\n", isVideoFile(extension))
-        if !seen[extension] {
-            seen[extension] = true
-            extensions = append(extensions, extension)
-        }
-    }
-    fmt.Printf("Extensions: %v\n", extensions)
-    return content, nil
+	extensions := []string{}
+	seen := map[string]bool{}
+	files, err := os.ReadDir(p)
+	if err != nil {
+		return nil, fmt.Errorf("Error raised: %w\n", err)
+	}
+	var content []FolderContent
+	for i, file := range files {
+		extension := filepath.Ext(file.Name())
+		fmt.Printf("File %d: %s, extension: %s\n", i, file.Name(), extension)
+		fmt.Printf("Verdict: %v\n", isVideoFile(extension))
+		if !seen[extension] {
+			seen[extension] = true
+			extensions = append(extensions, extension)
+		}
+	}
+	fmt.Printf("Extensions: %v\n", extensions)
+	return content, nil
 }
 
 func buildTrackList(p string) *TrackList {
@@ -149,20 +152,70 @@ func writePlayList(s any) error {
 	return nil
 }
 
+func testMetaData(p string) {
+	f, err := os.Open(p)
+	if err != nil {
+		log.Fatalf("Error raised: %s\n", err)
+	}
+	defer f.Close()
+	_, err = mp4.ReadBoxStructure(f, func(h *mp4.ReadHandle) (interface{}, error) {
+		if h.BoxInfo.Type.String() == "mvhd" {
+			fmt.Println("depth", len(h.Path))
+
+			// Box Type (e.g. "mdhd", "tfdt", "mdat")
+			fmt.Println("type", h.BoxInfo.Type.String())
+
+			// Box Size
+			fmt.Println("size", h.BoxInfo.Size)
+            box, _, err := h.ReadPayload()
+            if err != nil {
+                return nil, err
+            }
+			str, err := mp4.Stringify(box, h.BoxInfo.Context)
+            if err != nil {
+                fmt.Printf("Error raised: %s\n", err)
+                return nil, err
+            }
+            fmt.Println(str)
+		}
+
+		// if h.BoxInfo.IsSupportedType() {
+			// Payload
+			// box, _, err := h.ReadPayload()
+			// if err != nil {
+				// return nil, err
+			// }
+			// str, err := mp4.Stringify(box, h.BoxInfo.Context)
+			// if err != nil {
+				// return nil, err
+			// }
+			// f, _ := os.Create("temp.txt")
+			// buffer := bufio.NewWriter(f)
+			// buffer.WriteString(str)
+
+			// Expands children
+			// return h.Expand()
+		// }
+		return nil, nil
+	})
+}
+
 func main() {
 	path, err := getPath()
 	if err != nil {
 		log.Fatalf("Error raised: %s\n", err)
 	}
-    extensions := &[]string{}
-    seen := &map[string]bool{}
-    res := collectExtensions(path, extensions, seen)
-    fmt.Printf("Extensions: %v\n", *res)
-    // content, err := getFolderContent(path)
+	// extensions := &[]string{}
+	// seen := &map[string]bool{}
+	// res := collectExtensions(path, extensions, seen)
+	// fmt.Printf("Extensions: %v\n", *res)
+
+	testMetaData(path)
+	// content, err := getFolderContent(path)
 	// if err != nil {
-		// log.Fatalf("Error raised: %s\n", err)
+	// log.Fatalf("Error raised: %s\n", err)
 	// }
-    // fmt.Printf("Content: %v\n", content)
+	// fmt.Printf("Content: %v\n", content)
 	// trackList := buildTrackList(path)
 
 	// pl := &PlayList{Xmlns: Xmlns, XmlnsVlc: XmlnsVlc, Version: "1", Title: "Test playlist", Tl: *trackList}
